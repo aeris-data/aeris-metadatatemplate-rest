aa6129732e78a7407f4fa47504b389a9b2a44671
Matthieu Koskas <matthieu.koskas@obs-mip.fr>
2020-01-20 09:41:50 +0100

    Add tab in MetadataTemplate.java
    Add the visualisation tab in class MetadataTemplate, before populate the mock

--- src/main/java/fr/aeris/metadatatemplate/rest/domain/MetadataTemplate.java
+++ src/main/java/fr/aeris/metadatatemplate/rest/domain/MetadataTemplate.java
@@ -9,11 +9,52 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MetadataTemplate {
-
+	
 	private String name;
 	private List<String> metadataTab;
 	private List<String> dowloadTab;
 	private List<String> statisticsTab;
+	private List<String> visualisationTab;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getMetadataTab() {
+		return metadataTab;
+	}
+
+	public void setMetadataTab(List<String> metadataTab) {
+		this.metadataTab = metadataTab;
+	}
+
+	public List<String> getDowloadTab() {
+		return dowloadTab;
+	}
+
+	public void setDowloadTab(List<String> dowloadTab) {
+		this.dowloadTab = dowloadTab;
+	}
+
+	public List<String> getStatisticsTab() {
+		return statisticsTab;
+	}
+
+	public void setStatisticsTab(List<String> statisticsTab) {
+		this.statisticsTab = statisticsTab;
+	}
+
+	public List<String> getVisualisationTab() {
+		return visualisationTab;
+	}
+	
+	public void setVisualisationTab(List<String> visualisationTab) {
+		this.visualisationTab = visualisationTab;
+	}
 
 	public void addMetadataBlock(String blockName) {
 		if (metadataTab == null) {
@@ -27,6 +68,20 @@ public class MetadataTemplate {
 			dowloadTab = new ArrayList<>();
 		}
 		dowloadTab.add(blockName);
+	}
+	
+	public void addStatisticsBlock(String blockName) {
+		if (statisticsTab == null) {
+			statisticsTab = new ArrayList<>();
+		}
+		statisticsTab.add(blockName);
+	}
+	
+	public void addVisualisationBlock(String blockName) {
+		if (visualisationTab == null) {
+			visualisationTab = new ArrayList<>();
+		}
+		visualisationTab.add(blockName);
 	}
 
 }
